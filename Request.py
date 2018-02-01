29b83f8e064d9707673ab9565c23697fe4be7f02
maxqlord <lordmaxq@gmail.com>
2018-02-01 15:57:34 -0500

    Created python file to get all longitudes and latitudes

---
+++ Request.py
@@ -0,0 +1,21 @@
+import json, requests
+
+citylist = []
+with open('Cities') as inputfile:
+    for line in inputfile:
+        citylist.append(line)
+
+f = open('coordinates.txt','w')
+for location in range(0, len(citylist)):
+    resp = requests.get(url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + citylist[location] + "&key=AIzaSyAHTmTxlkasYVxQcFNKyhxb4JEuQ5oJSn4")
+    data = json.loads(resp.content)
+    latitude = data["results"][0]["geometry"]["location"]["lat"]
+    longitude = data["results"][0]["geometry"]["location"]["lng"]
+    f.write(str(latitude) + "," + str(longitude) + "#" + "\n")
+    print(str(location+1) + "  " + citylist[location] + " " + str(latitude) + "," + str(longitude) + "#")
+f.close()
+
+#get rid of 731
+
+
+
