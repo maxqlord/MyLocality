ad23fc9825a3ae566af3df2d91f3cbcdfe30b497
maxqlord <lordmaxq@gmail.com>
2018-02-13 09:40:38 -0500

    Finalized location data

--- Request.py
+++ Request.py
@@ -4,15 +4,14 @@ citylist = []
 with open('Cities') as inputfile:
     for line in inputfile:
         citylist.append(line)
-f = open('coordinates.txt','w')
-for location in range(len(citylist)):
-    resp = requests.get(url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + citylist[location] + "&key=AIzaSyAHTmTxlkasYVxQcFNKyhxb4JEuQ5oJSn4")
+
+for location in range(1190, len(citylist)):
+    resp = requests.get(url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + citylist[location] + "&key=AIzaSyCIRySZkmd2skHCd1QZtOtJgTB8JNn5cOA")
     data = json.loads(resp.content)
     latitude = data["results"][0]["geometry"]["location"]["lat"]
     longitude = data["results"][0]["geometry"]["location"]["lng"]
-    f.write(str(latitude) + "," + str(longitude) + "#" + "\n")
-    print(str(location+1) + "  " + citylist[location] + " " + str(latitude) + "," + str(longitude) + "#")
-f.close()
+    print(str(latitude) + "," + str(longitude) + "#")
+                    #print(str(location+1) + "  " + citylist[location] + " " + str(latitude) + "," + str(longitude) + "#")
 
 
 
